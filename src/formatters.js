938f0024a87d7673b48ed85fca45171d290e9b12
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2025-11-03 18:41:41 +0400

    ♻️ refactor: Remove slugify dependency
    Replaced slugify with a simpler string replacement to reduce bundle size and simplify the codebase. The previous implementation slugified the string, but the current use case only requires replacing specific characters.

--- src/formatters.js
+++ src/formatters.js
@@ -1,9 +1,9 @@
-import slugify from 'slugify' // link: <https://www.npmjs.com/package/slugify>
+// import slugify from 'slugify' // link: <https://www.npmjs.com/package/slugify>
 
 /**
  * @param {string} s
  * @return {string}
  */
 export const slug = (s) => {
-  return slugify(s.replace(/[._/()#+]/g, '-'), {replacement: '-', lower: true, strict: true})
+  return s.replace(/[._/()#+]/g, '-')
 }
